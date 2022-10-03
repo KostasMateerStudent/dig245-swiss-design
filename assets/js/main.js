64214e93e1e4f0fb80267fdd890b5cc174272819
KostasMateer <komateer@davidson.edu>
2022-10-03 12:25:36 -0400

    final design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,5 @@
-
 /* javascript */
+console.log(`Credit for original design: Swissted.com by Mike Joyce
+Credit for website: Kostas Mateer`);
+
+console.log(`Link to figma: https://www.figma.com/file/7TJZRAEohCpEbbWmylOa89/DIG-245-Swiss-Design?node-id=2%3A25`)
